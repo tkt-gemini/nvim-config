b4309542a085fa0b65402725cd8a122609a44c44
tkt-gemini <tkt310505@gmail.com>
2026-02-05 14:13:50 +0700

    fix: changed path for nvim

--- init.lua
+++ init.lua
@@ -4,16 +4,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-do
-	local home_local_bin = vim.fn.expand("~/.local/bin")
-	if
-		home_local_bin ~= ""
-		and not string.find(":" .. (vim.env.PATH or "") .. ":", ":" .. home_local_bin .. ":", 1, true)
-	then
-		vim.env.PATH = (vim.env.PATH or "") .. ":" .. home_local_bin
-	end
-end
-
 require("core.lazy")
 require("core.options")
 require("core.keymaps")
