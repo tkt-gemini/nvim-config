2474450d671fb2264bb2a8a1d36bcb4a8ac505e0
tkt-gemini <tkt310505@gmail.com>
2026-02-10 14:30:23 +0000

    feat: add ipynb.nvim plugin

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -25,4 +25,7 @@ require("lazy").setup({
 	change_detection = {
 		notify = false,
 	},
+  rocks = {
+    hererocks = false,
+  }
 })
--- lua/plugins/notebook.lua
+++ lua/plugins/notebook.lua
@@ -1,6 +1,7 @@
 -- ~/.config/nvim/lua/plugins/notebook.lua
 
 return {
+  -- plugin for .qmd file
   {
     "vhyrro/luarocks.nvim",
     priority = 1001,
@@ -13,21 +14,42 @@ return {
     "3rd/image.nvim",
     build = false,
     opts = {
-      backend = "sixel",
-      processor = "magick_cli",
-      max_width = 100,
-      max_height = 12,
-      max_width_window_percentage = math.huge,
-      max_height_window_percentage = math.huge,
+      backend = "sixel", -- "kitty" or "ueberzug" or "sixel"
+      processor = "magick_cli", -- or "magick_rock"
       integrations = {
         markdown = {
           enabled = true,
           clear_in_insert_mode = false,
           download_remote_images = true,
           only_render_image_at_cursor = false,
-          filetypes = { "markdown", "vimwiki", "quarto" },
+          only_render_image_at_cursor_mode = "popup", -- or "inline"
+          floating_windows = false, -- if true, images will be rendered in floating markdown windows
+          filetypes = { "markdown", "quarto", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+        },
+        neorg = {
+          enabled = true,
+          filetypes = { "norg" },
+        },
+        typst = {
+          enabled = true,
+          filetypes = { "typst" },
+        },
+        html = {
+          enabled = false,
+        },
+        css = {
+          enabled = false,
         },
       },
+      max_width = nil,
+      max_height = nil,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = 50,
+      scale_factor = 1.0,
+      window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
+      editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
     },
   },
 
@@ -36,6 +58,7 @@ return {
     version = "^1.0.0",
     build = ":UpdateRemotePlugins",
     dependencies = { "3rd/image.nvim" },
+    ft = { "quarto", "markdown" },
     init = function()
       vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_output_win_max_height = 20
@@ -70,16 +93,18 @@ return {
     "jmbuhr/otter.nvim",
     config = true,
   },
-  -- {
-  --   "ajbucci/ipynb.nvim",
-  --   dependencies = {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "neovim/nvim-lspconfig",
-  --     "nvim-tree/nvim-web-devicons",
-  --     "folke/snacks.nvim",
-  --   },
-  --   opts = {},
-  -- },
+
+  -- plugin for .ipynb file
+  {
+    "ajbucci/ipynb.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "neovim/nvim-lspconfig",
+      "nvim-tree/nvim-web-devicons", -- optional, for language icons
+      "folke/snacks.nvim", -- optional, for inline images
+    },
+    opts = {},
+  },
 
   {
     "folke/snacks.nvim",
